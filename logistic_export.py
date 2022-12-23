c13a30ed1f38eaeb21dab0edd07430ed07d6da26
NguyenThu2512 <93482603+NguyenThu2512@users.noreply.github.com>
2022-12-23 18:20:44 +0700

    up code cho 3 con bot jp_import, jp_export, vn_import

---
+++ logistic_export.py
@@ -0,0 +1,82 @@
+#%% nhập thư viện
+import warnings
+import pandas as pd
+warnings.filterwarnings('ignore')
+import time
+import pygsheets
+import numpy as np
+from IPython.display import Image, HTML
+#%% kết nối với file logistic_hub và file jp_export
+gc = pygsheets.authorize(service_file=r'Data\potent-orbit-371709-b58aa2a0a71c.json')
+#logistic_hub
+sh_logistic_hub=gc.open_by_key('1UYvnalxJyQBPdkds5X8v-bZF0Jz4xHXOlP7uwHD79E0')
+wks_logistic_hub=sh_logistic_hub.worksheet_by_title('logistic_hub')
+#jp_inventory
+sh_jp_inventory=gc.open_by_key('1GZ_lfj7XemTuXxGKMibKueo6wGfJ1wNv7oLMRP2BXTM')
+#jp_export
+wks_jp_export=sh_jp_inventory.worksheet_by_title('jp_export')
+#%% Tự động cập nhật dữ liệu
+while True:
+    try:
+        print("Đang cập nhật jp_export")
+        # lấy dữ liệu ở jp_export
+        jp_export_df=wks_jp_export.get_as_df(has_header=True)
+        # Lấy những cột cần thiết ở jp_export
+        true_logistic_data=jp_export_df[['package_id','product_id','jp_date_export']].copy().reset_index()
+        true_logistic_data['index']=true_logistic_data['index']+2
+        print("Đã lấy được dữ liệu từ jp_export ")
+
+        if(true_logistic_data.shape[0]==0):
+            print("Chưa có dữ liệu từ jp_export")
+            mn+1
+        else:
+            #Lấy những cột cần thiết ở jp_export
+            jp_export_df = wks_jp_export.get_as_df(has_header=True)
+            true_logistic_data = jp_export_df[['package_id', 'product_id', 'jp_date_export']].copy().reset_index()
+            true_logistic_data['index'] = true_logistic_data['index'] + 2
+            print("Đã lấy được dữ liệu từ jp_export ")
+
+            #Lọc dữ liệu có product_id giống nhau ở logistic_hub_df
+            product_id_export=true_logistic_data['product_id']
+            logistic_hub_df=wks_logistic_hub.get_as_df(has_header=True)
+            logistic_hub_df=logistic_hub_df.loc[(product_id_export.isin(logistic_hub_df['product_id']))].reset_index()
+            #Gắn index đối xừng với bảng sheet
+            logistic_hub_df['index']=logistic_hub_df['index']+2
+            #Cập nhật dữ liệu package_id và jp_date_export từ jp_export
+            for i in (logistic_hub_df['index']):
+                wks_logistic_hub.update_value(f'B{i}',true_logistic_data['package_id'][i-2])
+                wks_logistic_hub.update_value(f'G{i}', true_logistic_data['jp_date_export'][i - 2])
+    except NameError:
+        print("Đang cập nhật jp_export")
+        # lấy dữ liệu ở jp_export
+        jp_export_df = wks_jp_export.get_as_df(has_header=True)
+        # Lấy những cột cần thiết ở jp_export
+        true_logistic_data = jp_export_df[['package_id', 'product_id', 'jp_date_export']].copy().reset_index()
+        true_logistic_data['index'] = true_logistic_data['index'] + 2
+        print("Đã lấy được dữ liệu từ jp_export ")
+
+        if (true_logistic_data.shape[0] == 0):
+            print("Chưa có dữ liệu từ jp_export")
+            continue;
+        else:
+            # Lấy những cột cần thiết ở jp_export
+            jp_export_df = wks_jp_export.get_as_df(has_header=True)
+            true_logistic_data = jp_export_df[['package_id', 'product_id', 'jp_date_export']].copy().reset_index()
+            true_logistic_data['index'] = true_logistic_data['index'] + 2
+            print("Đã lấy được dữ liệu từ jp_export ")
+
+            # Lọc dữ liệu có product_id giống nhau ở logistic_hub_df
+            product_id_export = true_logistic_data['product_id']
+            logistic_hub_df = wks_logistic_hub.get_as_df(has_header=True)
+            logistic_hub_df = logistic_hub_df.loc[(product_id_export.isin(logistic_hub_df['product_id']))].reset_index()
+            # Gắn index đối xừng với bảng sheet
+            logistic_hub_df['index'] = logistic_hub_df['index'] + 2
+            # Cập nhật dữ liệu package_id và jp_date_export từ jp_export
+            for i in (logistic_hub_df['index']):
+                wks_logistic_hub.update_value(f'B{i}', true_logistic_data['package_id'][i - 2])
+                wks_logistic_hub.update_value(f'G{i}', true_logistic_data['jp_date_export'][i - 2])
+    finally:
+        time.sleep(5)
+
+
+
--- logistic_import.py
+++ logistic_import.py
@@ -1,14 +1,12 @@
-#%% import library
-
+#%% nhập thư viện
 import warnings
-
 import pandas as pd
 warnings.filterwarnings('ignore')
 import time
 import pygsheets
 from IPython.display import Image, HTML
-#%% import data
 
+#%% Kết nối với file Logistic Hub và  Japan Import
 gc = pygsheets.authorize(service_file=r'Data\potent-orbit-371709-b58aa2a0a71c.json')
 #logistic_hub
 sh_logistic_hub=gc.open_by_key('1UYvnalxJyQBPdkds5X8v-bZF0Jz4xHXOlP7uwHD79E0')
@@ -16,101 +14,112 @@ wks_logistic_hub=sh_logistic_hub.worksheet_by_title('logistic_hub')
 #jp_import
 sh_jp_inventory=gc.open_by_key('1GZ_lfj7XemTuXxGKMibKueo6wGfJ1wNv7oLMRP2BXTM')
 wks_jp_import=sh_jp_inventory.worksheet_by_title('jp_import')
-#jp_export
-wks_jp_export=sh_jp_inventory.worksheet_by_title('jp_export')
-#vn_import
-sh_vn_inventory=gc.open_by_key('1wW3ixtYOthX66jGrTpBqrnG5-MJWnfK1_h6u0tpTEl0')
-wks_vn_import=sh_vn_inventory.worksheet_by_title('vn_import')
-#%%
-#Lấy dữ liệu ở các cột cần thiết trong jp_import
-jp_import_df = wks_jp_import.get_as_df(has_header=True)
-jp_import_df_copy = jp_import_df[['tracking_id','product_id', 'product_name','product_image',
-                           'jp_date_received']].copy()
 
-existed_product_id = wks_logistic_hub.get_as_df(has_header=False)[3]
-true_logistic_data = jp_import_df_copy.loc[(~jp_import_df_copy['product_id'].isin(existed_product_id))].copy()
+while True:
+    try:
+        print("Đang lấy dữ liệu từ file jp_import")
+        jp_import_df = wks_jp_import.get_as_df(has_header=True)
+        jp_import_df_copy = jp_import_df[['tracking_id', 'product_id', 'product_name', 'product_image_link',
+                                          'jp_date_received']].copy()
 
-print("Đã lấy được dữ liệu từ jp_import")
+        existed_product_id = wks_logistic_hub.get_as_df(has_header=False)[3]
+        true_logistic_data = jp_import_df_copy.loc[(~jp_import_df_copy['product_id'].isin(existed_product_id))].copy()
 
-#%% Thay đổi vị trí cột và thêm giá trị rỗng
-empty_lst = [""] * true_logistic_data.shape[0]
-true_logistic_data['international_ship_fee'] = empty_lst
-true_logistic_data['payment_status']=empty_lst
-true_logistic_data['package_id']=empty_lst
-true_logistic_data['jp_date_export']=empty_lst
-true_logistic_data['vn_date_import']=empty_lst
-true_logistic_data['weight_rounded']=empty_lst
-true_logistic_data = true_logistic_data[wks_logistic_hub.get_row(1)]
+        print("Đã lấy được dữ liệu từ jp_import lần 1")
+        print("Lấy order lần 1")
 
-wks_logistic_hub_last_row = 1
-for x in wks_logistic_hub.get_col(5):
-    if x != "":
-        wks_logistic_hub_last_row += 1
-# thêm dòng trước khi viết trong trường hợp trang hết dòng
-print("Chuẩn bị ghi")
-wks_logistic_hub.add_rows(true_logistic_data.shape[0])
+        if true_logistic_data.shape[0] == 0:
+            print('Không có dữ liệu nào mới')
+            nm + 1
+        else:
+            print("Chờ 5s lỡ đâu tích nhầm")
+            time.sleep(5)
+            jp_import_df = wks_jp_import.get_as_df(has_header=True)
+            jp_import_df_copy = jp_import_df[['tracking_id', 'product_id', 'product_name', 'product_image_link',
+                                              'jp_date_received']].copy()
+            existed_product_id = wks_logistic_hub.get_as_df(has_header=False)[3]
+            true_logistic_data = jp_import_df_copy.loc[
+                (~jp_import_df_copy['product_id'].isin(existed_product_id))].copy()
+        print("Đã lấy được dữ liệu từ jp_import")
+        # Lấy hình ảnh tư jp_import
+        product_image = []
+        image_link = true_logistic_data['product_image_link']
+        for i in image_link:
+            x = f'=IMAGE("{i}")'
+            product_image.append(x)
+        #  Thay đổi vị trí cột và thêm giá trị rỗng
+        empty_lst = [""] * true_logistic_data.shape[0]
+        true_logistic_data['product_image'] = product_image
+        true_logistic_data['international_ship_fee'] = empty_lst
+        true_logistic_data['payment_status'] = empty_lst
+        true_logistic_data['package_id'] = empty_lst
+        true_logistic_data['jp_date_export'] = empty_lst
+        true_logistic_data['vn_date_import'] = empty_lst
+        true_logistic_data['weight_rounded'] = empty_lst
+        true_logistic_data = true_logistic_data[wks_logistic_hub.get_row(1)]
+        wks_logistic_hub_last_row = 1
+        for x in wks_logistic_hub.get_col(5):
+            if x != "":
+                wks_logistic_hub_last_row += 1
+        # thêm dòng trước khi viết trong trường hợp trang hết dòng
+        print("Chuẩn bị ghi")
+        wks_logistic_hub.add_rows(true_logistic_data.shape[0])
 
-wks_logistic_hub.set_dataframe(true_logistic_data, start=f"A{wks_logistic_hub_last_row}", copy_head=False, extend=True)
-logistic_hub_df=wks_logistic_hub.get_as_df(has_header=True)
+        wks_logistic_hub.set_dataframe(true_logistic_data, start=f"A{wks_logistic_hub_last_row}", copy_head=False,
+                                       extend=True)
+        logistic_hub_df = wks_logistic_hub.get_as_df(has_header=True)
 
-#%% lấy dữ liệu ở jp_export
+    except NameError:
+        print("Đang lấy dữ liệu từ file jp_import")
+        jp_import_df = wks_jp_import.get_as_df(has_header=True)
+        jp_import_df_copy = jp_import_df[['tracking_id', 'product_id', 'product_name', 'product_image_link',
+                                          'jp_date_received']].copy()
 
-jp_export_df=wks_jp_export.get_as_df(has_header=True)
-jp_export_df_copy=jp_export_df[['package_id','product_id','jp_date_export']].copy()
-true_logistic_data = jp_export_df_copy.copy()
-print("Đã lấy được dữ liệu từ jp_export")
+        existed_product_id = wks_logistic_hub.get_as_df(has_header=False)[3]
+        true_logistic_data = jp_import_df_copy.loc[(~jp_import_df_copy['product_id'].isin(existed_product_id))].copy()
 
-#%% Thay vị trí cột và thêm vào giá trị null
+        print("Đã lấy được dữ liệu từ jp_import lần 1")
+        print("Lấy order lần 1")
 
-empty_lst = [""] * true_logistic_data.shape[0]
-true_logistic_data['tracking_id']=logistic_hub_df['tracking_id']
-true_logistic_data['product_id']=logistic_hub_df['product_id'].astype(str)
-true_logistic_data['product_name']=logistic_hub_df['product_name']
-true_logistic_data['product_image']=logistic_hub_df['product_image']
-true_logistic_data['jp_date_received']=logistic_hub_df['jp_date_received']
-true_logistic_data['vn_date_import']=empty_lst
-true_logistic_data['weight_rounded']=empty_lst
-true_logistic_data['international_ship_fee'] = empty_lst
-true_logistic_data['payment_status']=empty_lst
-true_logistic_data = true_logistic_data[wks_logistic_hub.get_row(1)]
-true_logistic_data_copy=true_logistic_data.copy()
+        if true_logistic_data.shape[0] == 0:
+            print('Không có dữ liệu nào mới')
+            continue
+        else:
+            print("Chờ 5s lỡ đâu tích nhầm")
+            time.sleep(5)
+            jp_import_df = wks_jp_import.get_as_df(has_header=True)
+            jp_import_df_copy = jp_import_df[['tracking_id', 'product_id', 'product_name', 'product_image_link',
+                                              'jp_date_received']].copy()
+            existed_product_id = wks_logistic_hub.get_as_df(has_header=False)[3]
+            true_logistic_data = jp_import_df_copy.loc[
+                (~jp_import_df_copy['product_id'].isin(existed_product_id))].copy()
+        print("Đã lấy được dữ liệu từ jp_import")
+        # Lấy hình ảnh tư jp_import
+        product_image = []
+        image_link = true_logistic_data['product_image_link']
+        for i in image_link:
+            x = f'=IMAGE("{i}")'
+            product_image.append(x)
+        #  Thay đổi vị trí cột và thêm giá trị rỗng
+        empty_lst = [""] * true_logistic_data.shape[0]
+        true_logistic_data['product_image'] = product_image
+        true_logistic_data['international_ship_fee'] = empty_lst
+        true_logistic_data['payment_status'] = empty_lst
+        true_logistic_data['package_id'] = empty_lst
+        true_logistic_data['jp_date_export'] = empty_lst
+        true_logistic_data['vn_date_import'] = empty_lst
+        true_logistic_data['weight_rounded'] = empty_lst
+        true_logistic_data = true_logistic_data[wks_logistic_hub.get_row(1)]
+        wks_logistic_hub_last_row = 1
+        for x in wks_logistic_hub.get_col(5):
+            if x != "":
+                wks_logistic_hub_last_row += 1
+        # thêm dòng trước khi viết trong trường hợp trang hết dòng
+        print("Chuẩn bị ghi")
+        wks_logistic_hub.add_rows(true_logistic_data.shape[0])
 
-
-
-#%%Lấy dữ liệu từ vn_import
-vn_import_df = wks_vn_import.get_as_df(has_header=True)
-vn_import_df_copy = vn_import_df[['product_id','vn_date_import','weight_rounded']].copy()
-
-true_logistic_data = vn_import_df_copy.copy()
-print("Đã lấy được dữ liệu từ jp_export")
-
-#%% Thay đổi vị trí cột và thêm dữ liệu trống(phần này dữ liệu phần bên dưới xuất hiện null)
-
-empty_lst = [""] * true_logistic_data.shape[0]
-true_logistic_data['tracking_id']=logistic_hub_df['tracking_id']
-true_logistic_data['package_id']=true_logistic_data_copy['package_id']
-true_logistic_data['product_id']=logistic_hub_df['product_id'].astype(str)
-true_logistic_data['product_name']=logistic_hub_df['product_name']
-true_logistic_data['product_image']=logistic_hub_df['product_image']
-true_logistic_data['jp_date_received']=logistic_hub_df['jp_date_received']
-true_logistic_data['jp_date_export']=true_logistic_data_copy['jp_date_export']
-true_logistic_data['international_ship_fee'] = empty_lst
-true_logistic_data['payment_status']=empty_lst
-true_logistic_data = true_logistic_data[wks_logistic_hub.get_row(1)]
-
-#%% export data to logistic_hub
-wks_logistic_hub_last_row = 1
-for x in wks_logistic_hub.get_col(5):
-    if x != "":
-        wks_logistic_hub_last_row += 1
-# thêm dòng trước khi viết trong trường hợp trang hết dòng
-print("Chuẩn bị ghi")
-wks_logistic_hub.set_dataframe(true_logistic_data, start=f"A{wks_logistic_hub_last_row}", copy_head=False, extend=True)
-logistic_hub_df=wks_logistic_hub.get_as_df(has_header=True)
-
-#%%
-# logistic_hub_tong=pd.merge(jp_import_df_copy, jp_export_df_copy)
-# logistic_hub_tong=pd.merge(logistic_hub_tong,vn_import_df_copy)
-# df = jp_import_df_copy.merge(jp_export_df_copy).merge(vn_import_df_copy)
-
-
+        wks_logistic_hub.set_dataframe(true_logistic_data, start=f"A{wks_logistic_hub_last_row}", copy_head=False,
+                                       extend=True)
+        logistic_hub_df = wks_logistic_hub.get_as_df(has_header=True)
+    finally:
+        time.sleep(5)
