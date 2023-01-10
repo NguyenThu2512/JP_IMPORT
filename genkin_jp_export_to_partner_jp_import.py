0fbc3d8a9fa7df8593e34c2bd356abe113dff93b
NguyenThu2512 <93482603+NguyenThu2512@users.noreply.github.com>
2023-01-10 15:39:24 +0700

    Update genkin_jp_export_to_partner_jp_import.py

--- genkin_jp_export_to_partner_jp_import.py
+++ genkin_jp_export_to_partner_jp_import.py
@@ -1,3 +1,5 @@
+#Source code này được tạo ra nhằm tự động đổ dữ liệu từ file genkin_jp_export đến file partner_jp_import sử dụng thư viện chính là pygsheets
+
 #%% nhập thư viện
 import warnings
 import pandas as pd
@@ -15,34 +17,38 @@ wks_partner_export=sh_partner_inventory.worksheet_by_title('4. Partner Export')
 #jp_export
 sh_jp_inventory=gc.open_by_key('1GZ_lfj7XemTuXxGKMibKueo6wGfJ1wNv7oLMRP2BXTM')
 wks_jp_export=sh_jp_inventory.worksheet_by_title('4. Export')
-#%%
+#%% Sử dụng while True để chạy vòng lặp liên tục đổ dữ liệu ngay khi có sự thay đổi từ file gốc
 while True:
     # Đếm tổng số dòng hiện có trong sheet export
-    jp_export_last_row = 0
+    jp_export_last_row = 0 
     for x in wks_jp_export.get_col(1):
         if x != "":
             jp_export_last_row += 1
+    
+    #Lấy dữ liệu từ file gốc(genkin_jp_export) và chuyển sang định dạng dataframe
     print("Đang lấy dữ liệu từ file jp_export")
     jp_export_df = wks_jp_export.get_as_df(has_header=True, start=None, end=f'X{jp_export_last_row}')
     existed_product_id_import = wks_partner_import.get_as_df(has_header=False)[2]
     existed_product_id_inventory=wks_partner_inventory.get_as_df(has_header=False)[2]
     existed_product_id_export=wks_partner_export.get_as_df(has_header=False)[2]
-    cond1 = (~jp_export_df['product_id'].isin(existed_product_id_import))
-    cond2 = (~jp_export_df['product_id'].isin(existed_product_id_inventory))
-    cond3=(~jp_export_df['product_id'].isin(existed_product_id_export))
-    cond4 = (~(jp_export_df['package_id'] == "")
+    cond1 = (~jp_export_df['product_id'].isin(existed_product_id_import))    #Lấy điều kiện product_id không trùng với file đã được đổ từ file partner_import
+    cond2 = (~jp_export_df['product_id'].isin(existed_product_id_inventory)) #Lấy điều kiện product_id không trùng với file đã được đổ từ file partner_inventory
+    cond3=(~jp_export_df['product_id'].isin(existed_product_id_export))      #Lấy điều kiện product_id không trùng với file đã được đổ từ file partner_export
+    cond4 = (~(jp_export_df['package_id'] == "")                             #Lấy điều kiện cột không rỗng
              & ~(jp_export_df['lot_id'] == "")
              & ~(jp_export_df['partner_id'] == "")
              & ~(jp_export_df['jp_date_export'] == "")
              & (jp_export_df['transport'] == "Air KKI")
              & (jp_export_df['jp_export_confirm'] == "TRUE"))
-    true_partner_data = jp_export_df.loc[(cond1) & (cond2) & (cond3) & (cond4)].copy()
-    true_row = true_partner_data.shape[0]
+    true_partner_data = jp_export_df.loc[(cond1) & (cond2) & (cond3) & (cond4)].copy() # Dữ liệu đổ được lọc dữ liệu từ những điều kiện đã có 
     print("Đã lấy được dữ liệu từ jp_import")
+    
+    #Lấy số dòng hiện tại trong file được đổ(parter_jp_import) và kiểm tra số dòng
+    true_row = true_partner_data.shape[0]
     if true_row == 0:
         print('Không có dữ liệu nào mới')
         continue
-    # get last row of worksheet parter_import
+    # Lấy dòng cuối cùng của file parter_import (thỏa điều kiện toàn bộ dòng đều null)
     wks_partner_import_last_row_i = []
     lenth_row = len(wks_partner_import.get_row(1))
     for i in range(1, lenth_row):
@@ -53,22 +59,21 @@ while True:
         wks_partner_import_last_row_i.append(last_row)
     wks_partner_import_last_row_max = max(wks_partner_import_last_row_i)
 
-    #  Thay đổi vị trí cột và thêm giá trị rỗng
-    # Get image
+    # lấy hình ảnh từ file gốc
     product_image = []
     image_link = true_partner_data['jp_product_image_link']
     for i in image_link:
         x = f'=IMAGE("{i}")'
         product_image.append(x)
-    #
-    empty_lst = [""] * true_row
-    need_column = [x for x in wks_partner_import.get_row(1) if x not in wks_jp_export.get_row(1)]
+    #Thay đổi vị trí cột và thêm giá trị rỗng cho những cột không có trong file gốc
+    empty_lst = [""] * true_row        #Lấy giá trị rỗng với số hàng từ dữ liệu được đổ về(partner_import)
+    need_column = [x for x in wks_partner_import.get_row(1) if x not in wks_jp_export.get_row(1)]  #Lấy những cột từ file được đổ(partner_import) mà không có trong file gốc
     for x in need_column:
         true_partner_data[f'{x}'] = empty_lst
     true_partner_data['jp_product_image'] = product_image
     true_partner_data = true_partner_data[wks_partner_import.get_row(1)]
 
-    # thêm dòng trước khi viết trong trường hợp trang hết dòng và thực hiện ghi
+    #thực hiện ghi
     print("Chuẩn bị ghi")
-    wks_partner_import.add_rows(true_row)
+    wks_partner_import.add_rows(true_row)    #thêm dòng trước khi viết trong trường hợp trang hết dòng và
     wks_partner_import.set_dataframe(true_partner_data, start=f"A{wks_partner_import_last_row_max}", copy_head=False)
